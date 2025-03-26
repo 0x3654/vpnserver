3d3a2e1eb3a95fc22e153f1c351458bb622f3fab
tifinag <tifinag@users.noreply.github.com>
2025-03-26 18:02:27 +0300

    Добавлен скрипт установки 3x-ui

---
+++ 3x-ui/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Переход в корневую директорию
+cd /
+
+# Обновление системы
+apt update && apt full-upgrade -y
+
+# Установка необходимых пакетов
+apt install docker.io docker-compose git curl bash openssl -y
+
+# Клонирование репозитория
+git clone https://github.com/0x3654/nginx-3x-ui-subscription-proxy .
+
+# Генерация SSL сертификатов
+openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout private.key -out public.key -days 3650 -subj "/CN=APP"
+
+# Переход в директорию 3x-ui
+cd /3x-ui/
+
+# Запуск docker-compose
+docker-compose up -d
+
+# Установка WARP proxy
+bash <(curl -sSL https://raw.githubusercontent.com/hamid-gh98/x-ui-scripts/main/install_warp_proxy.sh) 
