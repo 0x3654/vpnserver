210fe1c79266d0ce89e79c84f1c1f32dfda122e1
tifinag <tifinag@users.noreply.github.com>
2025-03-27 16:45:05 +0300

    Исправлен скрипт update_config.sh: добавлена установка sqlite3 и безопасное обновление БД

--- install.sh
+++ install.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-
 # Переход в корневую директорию
 cd /
-
 # Обновление системы
 apt update && apt full-upgrade -y && apt install docker.io docker-compose git curl bash openssl htop -y
-
 # Клонирование репозитория
 git clone https://github.com/0x3654/xray-server 
-
 # Генерация SSL сертификатов
 openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout private.key -out public.key -days 3650
-
-
 # Запуск docker-compose в директории /3x-ui/
 cd /3x-ui/ && docker-compose up -d
-
-# Установка WARP proxy
-bash <(curl -sSL https://raw.githubusercontent.com/hamid-gh98/x-ui-scripts/main/install_warp_proxy.sh) 
+# Запуск WARP скрипта с параметрами
+./warp_install.sh -y -f
