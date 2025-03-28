684b9ed6d33ed20231cc5c73a4966762bfb2371b
tifinag <tifinag@users.noreply.github.com>
2025-03-28 15:21:26 +0300

    Обновление start.sh

--- start.sh
+++ start.sh
@@ -6,10 +6,10 @@ apt update && apt full-upgrade -y && apt install docker.io docker-compose curl b
 # Клонирование репозитория
 git clone https://github.com/0x3654/xray-server 
 # Генерация SSL сертификатов
-openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout private.key -out public.key -days 3650
+openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout private.key -out public.key -days 3650 -subj "/CN=localhost"
 chmod +x /xray-server/update_config.sh
 chmod +x /xray-server/3x-ui/warp_install.sh
 # Запуск docker-compose в директории /3x-ui/
 cd /xray-server/3x-ui/ && docker-compose up -d
 # Запуск WARP скрипта с параметрами
-./warp_install.sh -y -f
+./xray-server/warp_install.sh -y -f
