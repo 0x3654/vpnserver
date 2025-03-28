dcad42d64ad83e0f046ed161466c766a85805782
tifinag <tifinag@users.noreply.github.com>
2025-03-28 15:18:11 +0300

    Обновление конфигурации и добавление start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,11 @@
 # Переход в корневую директорию
 cd /
 # Обновление системы
-apt update && apt full-upgrade -y && apt install docker.io docker-compose git curl bash openssl htop vim sqlite3 ufw -y
+apt update && apt full-upgrade -y && apt install docker.io docker-compose curl bash openssl htop vim sqlite3 -y
 # Клонирование репозитория
 git clone https://github.com/0x3654/xray-server 
-
 # Генерация SSL сертификатов
 openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout private.key -out public.key -days 3650
-
 chmod +x /xray-server/update_config.sh
 chmod +x /xray-server/3x-ui/warp_install.sh
 # Запуск docker-compose в директории /3x-ui/
